0541043a96b31cac22b47c52f248265095710433
Ravikumar1133 <112365795+Ravikumar1133@users.noreply.github.com>
2022-11-13 17:41:26 +0530

    new proj
    university site

---
+++ form-handler.php
@@ -0,0 +1,20 @@
+<?php
+$name = $_post['name'];
+$visiors_email = $_post['email']
+$message = $_post['messege']
+
+$email_from ='';
+$email_subject ='new form';
+$email_body = "user name:$name.n/".
+              "user email:$email.n/".
+              "user messege:$messege.n/";
+$to = 'ravi333kumar333@gmail.com';
+$headers = "from:$email_from \r\n";
+
+$headers = "reply_to:$visitors_email \r\n";
+
+mail($to,$email_body,$headers);
+
+header("location: contact.html");
+
+?>
